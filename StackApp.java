6dab32533101f5b45226a597db7a3ca49c5c35c9
mrseay24 <sidneyeseay@gmail.com>
2019-12-18 20:14:53 -0500

    Add files via upload

---
+++ StackApp.java
@@ -0,0 +1,20 @@
+import java.util.*;
+public class StackApp {
+	Stack<Character> stack = new java.util.Stack<>();
+	Scanner sc = new Scanner(System.in);
+	String x;
+	String y;
+	
+	String test = x = y – (z + w) * (v + z); 
+public static void main(String[]args){
+	String openDelim = "("+"{"+"["+"/*";
+	String closeDelim = ")"+"}"+"]";
+	if(openDelim != null){
+		stack.push();
+	}
+	else(closeDelim){
+		
+	}
+}
+sc.close();
+}
